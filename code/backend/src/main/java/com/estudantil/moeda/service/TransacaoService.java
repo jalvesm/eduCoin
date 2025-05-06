7a81e8399f31e8ef1d498b6f63a7eeffc284a31c
Joana Morais <joanaalvesm@gmail.com>
2025-05-06 00:46:58 -0300

    fix: resolve método buscarTransacoesPorEmpresa;

--- code/backend/src/main/java/com/estudantil/moeda/service/TransacaoService.java
+++ code/backend/src/main/java/com/estudantil/moeda/service/TransacaoService.java
@@ -1,10 +1,14 @@
 package com.estudantil.moeda.service;
 
 import com.estudantil.moeda.model.Transacao;
+import com.estudantil.moeda.model.Usuario;
 import com.estudantil.moeda.repository.TransacaoRepository;
+import com.estudantil.moeda.repository.UsuarioRepository;
+import com.estudantil.moeda.dto.ResponseTransactionByEmpresaDTO;
 import com.estudantil.moeda.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +18,10 @@ import java.util.UUID;
 @Service
 public class TransacaoService {
 
+    @Autowired
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
     private final TransacaoRepository transacaoRepository;
 
     public List<Transacao> findAll() {
@@ -43,4 +51,36 @@ public class TransacaoService {
         }
         transacaoRepository.deleteById(id);
     }
-} 
+
+    /*
+     * public List<Transacao> buscarTransacoesPorEmpresa(UUID empresaId) {
+     * Usuario empresa = usuarioRepository.findById(empresaId)
+     * .orElseThrow(() -> new ResourceNotFoundException("Empresa não encontrada"));
+     * 
+     * return transacaoRepository.findByRemetenteOrDestinatario(empresa, empresa);
+     * }
+     */
+
+    public List<ResponseTransactionByEmpresaDTO> buscarTransacoesPorEmpresa(UUID empresaId) {
+        Usuario empresa = usuarioRepository.findById(empresaId)
+                .orElseThrow(() -> new ResourceNotFoundException("Empresa não encontrada"));
+
+        List<Transacao> transacoes = transacaoRepository.findByRemetenteOrDestinatario(empresa, empresa);
+
+        return transacoes.stream()
+                .map(transacao -> {
+                    Usuario cliente = transacao.getRemetente().equals(empresa)
+                            ? transacao.getDestinatario()
+                            : transacao.getRemetente();
+
+                    return new ResponseTransactionByEmpresaDTO(
+                            transacao.getId(),
+                            transacao.getDescricao(),
+                            transacao.getValor(),
+                            transacao.getDataTransacao(),
+                            cliente.getNome());
+                })
+                .toList();
+    }
+
+}
