20b21f2bd390b20f660b06f83e3477642e94486c
erica-as <erica.asnt@hotmail.com>
2025-06-03 21:12:16 -0300

    adicionando footer

--- code/frontend/educoin/src/shared/components/Footer/Footer.tsx
+++ code/frontend/educoin/src/shared/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
-import { Box, Typography, Link, Container } from "@mui/material";
+import { Box, Typography, Container } from "@mui/material";
 import React from "react";
+import colors from "../../theme/colors";
 
 const Footer = () => {
   return (
     <Box
       component="footer"
       sx={{
-        backgroundColor: "#f5f5f5",
+        backgroundColor: colors.primary,
         borderTop: "1px solid #e0e0e0",
         mt: "auto",
         py: 3,
@@ -15,18 +16,6 @@ const Footer = () => {
       <Container maxWidth="lg" sx={{ textAlign: "center" }}>
         <Typography variant="body2" color="textSecondary">
           © {new Date().getFullYear()} EduCoin. Todos os direitos reservados.
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          Desenvolvido por{" "}
-          <Link
-            href="https://seuportifolio.com"
-            color="inherit"
-            underline="hover"
-            target="_blank"
-            rel="noopener"
-          >
-            Seu Nome
-          </Link>
         </Typography>
       </Container>
     </Box>
